a7dfe6a4a4a009d506d14da35a5222e7e4141351
Kenneth Yuen <kenneth.x.yuen@gmail.com>
2018-11-18 12:49:17 -0600

    fixed to run with nodemon

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+'use strict';
+
 const hapi = require('hapi');
 
+// Create a server with a host and port
 const server = hapi.server({
     port: 3000,     // for code anywhere to be exposed
     host: '0.0.0.0' // yes, this is opened to the public.  it is a sandbox! 
     //host: 'localhost'
 })
 
-const init = async () => {
-    await server.start();
-    console.log('Server running at: ${server.info.uri}');
-};
+// Hone Route
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request,reply) => {
+        reply('Hello World');
+    }
+});
 
-init();
+// Start the server
+server.start((err) => {
+    if(err){
+        throw err;
+    }
+
+    console.log(`Server started at: ${server.info.uri}`);
+});
+
